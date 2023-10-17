68028622903f8264957ad60a8459cb615329ec16
Igor Degtyarenko <div50015@mail.ru>
2023-10-17 15:02:14 +0300

    commit mid-level-step-objects branch

--- tests/test_demoqa_operations.py
+++ tests/test_demoqa_operations.py
@@ -1,34 +1,32 @@
 import time
 from selene import browser, have
 import os
-from selene import command
+
+from lesson10_hw.pages.registration_page import RegistrationPage
+
 
 def test_complete_todo():
-    browser.open('/')
-    # уменьшение размера изображения в 0.8 раза
-    browser.execute_script('document.querySelector(".body-height").style.transform = "scale(.8)"')
-    # ожидание в течение 10 секунд 3х реклпмных банеров и их удаление (вторая строка)
-    # browser.all('[id^google_ads][id$=container__]').with_(timeout=10).wait_until(have.size_less_than_or_equal(3))
-    # browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
+    registration_page = RegistrationPage()
+    registration_page.open()
 
-    browser.element('#firstName').type('Ivan')
-    browser.element('#lastName').type('Petrov')
-    browser.element('#userEmail').type('petrov@mail.ru')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').type('79287777777')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__year-select').type('1999')
-    browser.element('.react-datepicker__month-select').type('August')
-    browser.element('.react-datepicker__day--009').click()
-    browser.element('#subjectsInput').type('Biology').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element("#uploadPicture").send_keys(os.path.abspath('files/ball.jpg'))
-    browser.element('#currentAddress').type('Russia Moscow')
-    browser.element('#react-select-3-input').type('NCR').press_enter()
-    browser.element('#react-select-4-input').type('Noida').press_enter()
-    browser.element('#submit').click()
-    time.sleep(10)
-    browser.element('.table').all('tr td:nth-child(2)').should(have.exact_texts(
+    # WHEN
+    registration_page.type_first_name('Ivan')
+    registration_page.type_last_name('Petrov')
+    registration_page.type_email('petrov@mail.ru')
+    registration_page.click_gender('Male')
+    registration_page.type_number('79287777777')
+    registration_page.type_date_of_birth('1999', 'August', '09')
+    registration_page.type_subjects('Biology')
+    registration_page.click_hobbies('Sports')
+    registration_page.download_picture('files/ball.jpg')
+    registration_page.type_address('Russia Moscow')
+    registration_page.type_state('NCR')
+    registration_page.type_city('Noida')
+    registration_page.click_submit('#submit')
+    # time.sleep(10)
+
+    # THEN
+    registration_page.registered_user_data_should(
         'Ivan Petrov',
         'petrov@mail.ru',
         'Male',
@@ -38,4 +36,5 @@ def test_complete_todo():
         'Sports',
         'ball.jpg',
         'Russia Moscow',
-        'NCR Noida'))
+        'NCR Noida',
+    )
