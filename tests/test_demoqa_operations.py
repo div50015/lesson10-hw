b6fcb81f50b9be8f717dfa40fe6593b2fcc19478
Igor Degtyarenko <div50015@mail.ru>
2023-10-19 13:58:40 +0300

    error correction mid-level-step-objects branch

--- tests/test_demoqa_operations.py
+++ tests/test_demoqa_operations.py
@@ -1,8 +1,8 @@
 import time
 from selene import browser, have
 import os
-
 from lesson10_hw.pages.registration_page import RegistrationPage
+from pathlib import Path
 
 
 def test_complete_todo():
@@ -18,7 +18,9 @@ def test_complete_todo():
     registration_page.type_date_of_birth('1999', 'August', '09')
     registration_page.type_subjects('Biology')
     registration_page.click_hobbies('Sports')
-    registration_page.download_picture('files/ball.jpg')
+    registration_page.download_picture(
+        Path(__file__).parent.parent.joinpath(f'tests/files/ball.jpg'))
+    # print(Path(__file__).parent.parent.joinpath(f'tests/files/ball.jpg'))
     registration_page.type_address('Russia Moscow')
     registration_page.type_state('NCR')
     registration_page.type_city('Noida')
